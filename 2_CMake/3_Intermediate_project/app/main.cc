a0389dc4c3fccbe5910b4b318811275aee6c379d
zardykhan <45559226+zardykhan@users.noreply.github.com>
2022-09-29 16:35:22 +0200

    nested CMakes investigated

---
+++ 2_CMake/3_Intermediate_project/app/main.cc
@@ -0,0 +1,7 @@
+#include "my_lib.h"
+#include <iostream>
+int main()
+{
+    print_hello_world();
+    return 0;
+}
---
+++ 2_CMake/3_Intermediate_project/src/my_lib/my_lib.cc
@@ -0,0 +1,8 @@
+#include "my_lib.h"
+
+#include <iostream>
+
+void print_hello_world()
+{
+    std::cout << "Hello World!\n";
+}
